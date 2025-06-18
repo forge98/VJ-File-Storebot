efba4e99c862a5e2b56ec95154b97ccfea8a61bf
Forge9988 <dsgskgaming@gmail.com>
2025-06-18 16:26:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "22739755"))
 API_HASH = environ.get("API_HASH", "43b9c57bd7daba8a3c2a49e4b7149156")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7992045277:AAHuKotnK1-h_hQSX-eSoE2wzpqMBwvN8Hs")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8072927810').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "For4u_hub_bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
